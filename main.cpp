73f45ecb8f242e90a5af6967f5d110be49c8773b
Ashher Ali <99893402+ASQUAREEE@users.noreply.github.com>
2023-12-28 13:25:10 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -546,8 +546,8 @@ int main()
   cout << "_*_*_*_*_*_*_*_*_*__*_*_*_*_*WELCOME TO OS-SOME_*_*_*_*_*__*_*_*_*_*_*_\n";
   cout << "\n\nPROJECT BY:\n";
   cout << "---------------------\n";
-  cout << "2.Ashher Ali(captain)\n";
-  cout << "1.Shivam Sharma\n";
+  cout << "1.Ashher Ali(captain)\n";
+  cout << "2.Shivam Sharma\n";
   cout << "3.Asutosh Pradhan\n";
   cout << "4.Shresth\n";
   cout << "\n\n\n\n\nLoading have patient and read the credits...";
